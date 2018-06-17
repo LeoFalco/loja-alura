8149730c9f99619ff4cce922f1de0dd7acd6c67e
Leonardo Jesus Falco <leonardo.falco@outlook.com>
2018-06-17 20:49:26 -0300

    modificado local do diretorio views

--- config/express.js
+++ config/express.js
@@ -2,6 +2,6 @@ module.exports = function () {
     var express = require("express");
     var app = express();
     app.set("view engine", "ejs");
-    
+    app.set("views", "./app/views");
     return app;
 }

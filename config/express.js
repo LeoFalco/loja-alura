ac9d183616f8aa01bd08d9ebf49840670dc50263
Leonardo Jesus Falco <leonardo.falco@sifat.com.br>
2018-06-27 12:00:58 -0300

    emable cors

--- config/express.js
+++ config/express.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const load = require("express-load");
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 module.exports = function () {
     const app = express();
 
-    // definir bodyparser antes das rotas
-    app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-    app.use(bodyParser.json());
-
-    app.use(function(req, res, next) {
+    app.use(cors());
+    /*
+    app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
         next();
     });
+    */
+    // definir bodyparser antes das rotas
+    app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+    app.use(bodyParser.json());
 
-    load("routes", { cwd: "app" })
+
+    load("routes", {cwd: "app"})
         .then("infra")
         .into(app);
 
